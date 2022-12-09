dbe1927ace696f4de395e7007b779434293f9c08
Patrick Davlin <patrick.davlin@sharplink.com>
2022-12-09 10:08:32 -0600

    day 6

--- pages/solutions/day6.tsx
+++ pages/solutions/day6.tsx
@@ -1,13 +1,37 @@
+import { arrayBuffer } from "stream/consumers";
 import SolutionContainer from "../../components/SolutionContainer";
 
 // set example input here
-const default1 = null;
+const default1 = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`;
 const default2 = default1;
 
-export default function Day6() {
-  const p1 = null;
+const getMarkerLocation = (input, markerLen) =>
+  input.length -
+  input.split(
+    input
+      .trimEnd()
+      .split("")
+      .reduce((acc: string[], char: string, index: number) => {
+        if (acc.length < markerLen) {
+          acc.push(char);
+        } else if (
+          acc.length >
+          acc.filter((val, i, self) => self.indexOf(val) === i).length
+        ) {
+          acc.push(char);
+        }
+        if (acc.length > markerLen) {
+          acc.shift();
+        }
+        return acc;
+      }, [])
+      .join("")
+  )[1].length;
 
-  const p2 = null;
+export default function Day6() {
+  const p1 = (input) => getMarkerLocation(input, 4);
+
+  const p2 = (input) => getMarkerLocation(input, 14);
 
   return (
     <SolutionContainer
